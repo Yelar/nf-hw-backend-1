d18bcd4138646ca3ac1b37eaa0f1cb38acbe7815
Yelarys <100526663+Yelar@users.noreply.github.com>
2024-06-12 13:58:47 +0500

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://Yelarys:zh7xVDU_Av7NPJU@backend.aygrmmc.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=backend');
+        await mongoose.connect(process.env.MONGODB_URL || '');
         console.log('MongoDB connected...');
     } catch (err:any) {
         console.error(err.message);
